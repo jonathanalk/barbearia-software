7cbbbbf89cf4a43df6f0651d49feed3a14a8e9d9
Jonathan <nikejon12@hotmail.com>
2024-09-18 18:05:41 -0300

    3rd commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-//import logo from './logo.svg';
 import './App.css';
 import { createRoot } from 'react-dom/client';
 import axios from 'axios';
@@ -24,24 +23,6 @@ function App() {
       </div>
     )
   } )
-  // (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  // );
 }
 
 
